d626628caa25e480d8971e7db02e7f67236b13cb
Gleb Royako <groyako2003@gmail.com>
2026-05-06 11:38:20 +0300

    Create compute _compute_display_name, and fix template

--- add_car_to_product/models/product_product.py
+++ add_car_to_product/models/product_product.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductProduct(models.Model):
@@ -19,3 +19,30 @@ class ProductProduct(models.Model):
     for_all_models = fields.Boolean('For all models')
     oem = fields.Char('OEM')
     ovoko_part_id = fields.Char('Ovoko part ID')
+
+    @api.depends(
+        'product_tmpl_id.name',
+        'is_autoparts',
+        'compatible_vehicle_ids',
+        'compatible_vehicle_ids.brand_id',
+        'compatible_vehicle_ids.model_type',
+        'compatible_vehicle_ids.model_year_from',
+        'compatible_vehicle_ids.model_year_to',
+    )
+    def _compute_display_name(self):
+        autoparts = self.filtered(lambda p: p.is_autoparts and p.compatible_vehicle_ids)
+        super(ProductProduct, self - autoparts)._compute_display_name()
+        for product in autoparts:
+            vehicle = product.compatible_vehicle_ids[0]
+            parts = [product.product_tmpl_id.name or '']
+            if vehicle.brand_id.name:
+                parts.append(vehicle.brand_id.name)
+            if vehicle.name:
+                parts.append(vehicle.name)
+            if vehicle.model_type:
+                parts.append(vehicle.model_type)
+            if vehicle.model_year_from or vehicle.model_year_to:
+                year_from = str(vehicle.model_year_from) if vehicle.model_year_from else ''
+                year_to = str(vehicle.model_year_to) if vehicle.model_year_to else ''
+                parts.append(f"{year_from}-{year_to}".strip('-'))
+            product.display_name = ' '.join(filter(None, parts))
