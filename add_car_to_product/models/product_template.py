c28a1f18696ce46ac89f386dd1ed4c6788f0a586
Gleb Royako <groyako2003@gmail.com>
2026-05-06 11:19:00 +0300

    Fix autoparts chekbox because not see variants

--- add_car_to_product/models/product_template.py
+++ add_car_to_product/models/product_template.py
@@ -5,3 +5,14 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     is_autoparts = fields.Boolean('Is Autopart')
+
+    def action_open_autopart_variants(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Autopart Variants',
+            'res_model': 'product.product',
+            'view_mode': 'list,form',
+            'domain': [('product_tmpl_id', '=', self.id)],
+            'context': {'default_product_tmpl_id': self.id},
+        }
